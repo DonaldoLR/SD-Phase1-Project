1404c5d7003c01a35cd74b5828918c5dacd02b78
github username <github email address>
2021-04-20 20:15:38 -0400

    Displayed the joke div in the DOM

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+// Step 1, get the data
+// Fetchs the Data
 function fetchJoke(){
     const baseUrl = `https://official-joke-api.appspot.com/jokes`;
 
     // Creates a variable to hold the search genre input
     let searchTopic = ` `;
 
+    // Output: A promise with the data formatted to JSON
     return fetch(`${baseUrl}/random`).then(r => r.json())
 }
-/*
+
+// Step 2 handling the data 
+// Step 2.1 How I was the joke to be structured in HTML
+/* 
 <div id="joke">
     <h1>What is a centipedes's favorite Beatle song?</h1>
     <button>I don't know, what?</button>
     <h2>I want to hold your hand, hand, hand, hand...</h2>
 </div>
 */
+// Step 2.2 creating a function that takes in an object containing the joke
+// Output: A div that can be used on the DOM
 function createJokeDiv(jokeObj){
 
     const setupLine = jokeObj.setup;
@@ -34,6 +42,11 @@ function createJokeDiv(jokeObj){
     return createDiv; 
 }   
 
+function displayJoke(div) {
+    return document.body.appendChild(div);
+}
+
 fetchJoke()
     .then(createJokeDiv)
+    .then(displayJoke)
     
